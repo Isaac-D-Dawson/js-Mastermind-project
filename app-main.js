053204513e13fa0bce73995a10acb63605dfde11
Isaac-D-Dawson <isaacdanieldawson1837@gmail.com>
2022-10-05 16:26:18 +0100

    Updating index to have support for game controls.

--- app-main.js
+++ app-main.js
@@ -4,6 +4,16 @@ let compGuess   = ["red", "green", "blue", "yellow"];
 let playGuess   = ["red", "yellow", "white", "green"];
 
 
+/**
+ * takes in two iterables of equal length and returns an iterable containing data about the seocnd list compared to the first.
+ * items present in both with the same index are tagged "match"
+ * Items present in both lists but with differe indeces are tagged "present"
+ * items not present in both lists are tagged "wrong"
+ * @param {iterable} com_guess 
+ * @param {iterable} plr_guess 
+ * @returns iterable
+ * @error   false
+ */
 const compareGuesses    = (com_guess, plr_guess) => {
     if (com_guess.length != plr_guess.length) {
         console.error("Error, Guesses are not of equal length");
